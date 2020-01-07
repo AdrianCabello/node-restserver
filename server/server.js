977448c19324caf0feaf70f75132632a93c8d2d9
Adrian Cabello <acabello@symplr.com>
2020-01-07 11:19:46 -0300

    Rest server -usuario

--- server/server.js
+++ server/server.js
@@ -1,52 +1,25 @@
 require('./config/config')
 
-const express = require('express')
-const app = express()
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function (req, res) {
-    res.json('Hello World')
-})
+app.use(require('./routes/usuario'))
 
-app.post('/usuario', function (req, res) {
-
-    let persona = req.body;
-
-    if (persona.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es nesario'
-        });
-    } else {
-        res.json({
-            persona
-        })
-    }
-})
-
-app.put('/usuario/:id', function (req, res) {
-
-    let id = req.params.id;
-
-    console.log(req)
-
-    res.json(
-        {
-            id
-        }
-    )
-})
-
-app.delete('/usuario', function (req, res) {
-    res.json('Hello World')
-})
+mongoose.connect('mongodb://localhost:27017/cafe', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => console.log('DB Connected!')).catch(err => {
+    console.log(err.message);
+});
 
 app.listen(process.env.PORT, () => {
-    console.log('Escuchando puerto: ', process.env.PORT)
+    console.log('Escuchando puerto: ', process.env.PORT);
 })
